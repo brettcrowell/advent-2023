0fdd30c805c75618910234e0c5ad206dd4da60be
Brett Crowell <brettcrowell@gmail.com>
2023-12-02 15:50:12 -0500

    puzzle01;part2

--- lib/puzzle01.ex
+++ lib/puzzle01.ex
@@ -2,13 +2,33 @@ defmodule Puzzle01 do
   @moduledoc """
   Documentation for `Advent2023`.
   """
+
+  def to_integer(number) do
+    case number do
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+      _ -> number
+    end
+  end
+
   def sum_first_last(input) do
-    matches = Regex.scan(~r/\d/, input)
+    matches =
+      Regex.scan(
+        ~r/(\d|(?=((one)))|(?=(two))|(?=(three))|(?=(four))|(?=(five))|(?=(six))|(?=(seven))|(?=(eight))|(?=(nine)))/,
+        input
+      )
 
-    [first] = List.first(matches)
-    [last] = List.last(matches)
+    first = List.last(List.first(matches))
+    last = List.last(List.last(matches))
 
-    String.to_integer(first <> last)
+    String.to_integer(to_integer(first) <> to_integer(last))
   end
 
   def part1(input) do
--- test/puzzle01_test.exs
+++ test/puzzle01_test.exs
@@ -8,6 +8,16 @@ defmodule Puzzle01Test do
       assert 15 == Puzzle01.sum_first_last("a1b2c3d4e5f")
       assert 77 == Puzzle01.sum_first_last("treb7uchet")
     end
+
+    test "should understand written integers" do
+      assert 29 == Puzzle01.sum_first_last("two1nine")
+      assert 83 == Puzzle01.sum_first_last("eightwothree")
+      assert 13 == Puzzle01.sum_first_last("abcone2threexyz")
+      assert 24 == Puzzle01.sum_first_last("xtwone3four")
+      assert 42 == Puzzle01.sum_first_last("4nineeightseven2")
+      assert 14 == Puzzle01.sum_first_last("zoneight234")
+      assert 76 == Puzzle01.sum_first_last("7pqrstsixteen")
+    end
   end
 
   describe "#part1" do
@@ -18,6 +28,18 @@ a1b2c3d4e5f
 treb7uchet"
 
       assert 142 == Puzzle01.part1(input)
+    end
+
+    test "should handle written numbers" do
+      input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"
+
+      assert 281 == Puzzle01.part1(input)
     end
   end
 end
